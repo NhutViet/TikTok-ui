3f29e3f856db55e059583910a665cce39b1fd2e9
NhutViet <156928605+NhutViet@users.noreply.github.com>
2024-08-29 08:50:49 +0700

    Layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,19 @@
+// layout
+import { HeaderOnly } from '~/component/Layout';
+
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
 
 // không đăng nhập có thể xem được App
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
     { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
 ];
 // đăng nhập có thể xem được App
 const privateRoutes = [];
