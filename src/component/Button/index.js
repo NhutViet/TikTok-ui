e1d1bc4834bfa46f6aecab059cdbf909178f1fdd
NhutViet <156928605+NhutViet@users.noreply.github.com>
2024-08-31 14:09:00 +0700

    header

---
+++ src/component/Button/index.js
@@ -0,0 +1,60 @@
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+import { Link } from 'react-router-dom';
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    small = false,
+    large = false,
+    text = false,
+    disable = false,
+    rounded = false,
+    leftIcon = false,
+    children,
+    className,
+    onClick,
+    ...passProps
+}) {
+    let Comp = 'button';
+    const props = {
+        onClick,
+        ...passProps,
+    };
+
+    if (disable) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = 'a';
+    }
+    let classes = cx('wrapper', {
+        [className]: className,
+        primary,
+        outline,
+        small,
+        large,
+        text,
+        disable,
+        rounded,
+    });
+
+    return (
+        <Comp className={classes} {...props}>
+            <span>{children}</span>
+        </Comp>
+    );
+}
+
+export default Button;
